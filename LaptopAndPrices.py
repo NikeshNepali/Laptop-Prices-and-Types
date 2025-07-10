d13047046cc45502334a089cb8de8a624acdb858
NikeshNepali <nepalinikesh48@gmail.com>
2025-07-09 23:53:32 -0500

    Day 2
    In this Work I learned today how to make a Barchart with seaborn by importing it. Cleaned some more data from before and made a barchart using seaborn label it appropriately. I learned to use ax.text(), Understood how str replace regex works,  Understood how the loc works. Learend how to delete a column. Learned how to use a single column and a series to construct a graph at sns by renaming the column.

--- LaptopAndPrices.py
+++ LaptopAndPrices.py
@@ -1,5 +1,7 @@
 # Importing the required modules.
 import pandas as pd
+import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 from kaggle.api.kaggle_api_extended import KaggleApi
 
@@ -35,31 +37,65 @@ for col in df.columns:
 
 print(df['Weight'].unique()) # Checking
 
+
 # Cleaning data Starts.
+
+# Assigning the Operating system into each of it
+df['OpSys'] = df['OpSys'].str.capitalize()
+df['OpSys'] = df['OpSys'].str.replace("Macos|Mac os x", "Mac OS",regex=True).str.strip()
+df.loc[df['OpSys'].isin(["Windows 10", "Windows 7","Windows 1", "Windows 10 s"]),"OpSys"] = "Windows OS"
+
 df['Weight'] = df['Weight'].str.replace(r'[^\d\.]', '', regex=True)  # Keep only digits and dot
 df['Weight'] = df['Weight'].str.strip() # Removes empty spaces.
 df['Weight'] = pd.to_numeric(df['Weight'], errors='coerce') # converts values to numeric and replaces null for empty value
 
 # Cleaning ScreenResolution data
 df['ScreenResolution'] = df['ScreenResolution'].str.replace(r'\d+|x', '', regex=True).str.strip() # Removes numbers and x
-df['ScreenResolution'] = df['ScreenResolution'].str.replace('K', '4K') # Re arranging the Valid value
+df['ScreenResolution'] = df['ScreenResolution'].str.replace('K', '4K').str.strip() # Re arranging the Valid value
+df['ScreenResolution'] = df['ScreenResolution'].replace('', "Unkown")
 
 # Checking the result. 
 print(df['ScreenResolution'].unique())
 
 # Cleaning the Memory Data
+# Expanding the table
 df[['Storage','Storage Type']] = df['Memory'].str.split(" ",n=1, expand= True)
+# Result of both the expanded tables.
+df['Storage'] = df['Storage'].str.replace('[^a-zA-Z0-9|.]'," ", regex=True).str.strip()
+df.loc[df['Storage Type'].str.contains(r"\+",case=False, na= False),'Storage Type'] = "Dual"
 print(df['Storage'].unique())
 print(df['Storage Type'].unique())
 
 # Removing the empty value
 df = df.dropna()
+df = df.drop(columns=["Memory"])
 # Changing the required data Types.
 df['Weight'] = df['Weight'].astype(float)
 print(df.isnull().sum())
 
 # Cleaned Data Types
 print(df.dtypes)
+print(df.tail())
 
-# Changing Data Types
-print(df.dtypes)
+
+# Now making the necessary graphs.
+MostOSUsed = df['OpSys'].value_counts().reset_index()
+MostOSUsed.columns = ['OpSys','Count']
+
+# Using seaborn for barchart plotting and label.
+ax = sns.barplot(x= 'OpSys', y ='Count' ,data=MostOSUsed, color = 'green' )
+for bar in ax.patches:
+    height = bar.get_height()
+    ax.text(bar.get_x() + bar.get_width()/2,
+            height,
+            f'{height:.0f}',
+            ha = 'center',
+            va = 'bottom') 
+
+plt.title("Most Used Operating System")
+plt.xlabel("OpSys")
+plt.ylabel("Counts")
+plt.tight_layout()
+
+plt.show()
+
